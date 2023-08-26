b1529546173d418ed7ff7eef325e63efe03477e7
Olga <workpost.mkh@yandex.ru>
2023-08-27 00:22:09 +0300

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// написать программу которая по заданному номеру четверти показывает диапазон возможных координат точек этой четверти
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+void QuarterAvailable(int quarter)
+{
+    if (quarter == 1)
+    {
+       System.Console.WriteLine("x > 0, y > 0"); 
+    }
+    else if (quarter == 2)
+    {
+        System.Console.WriteLine("x < 0, y > 0");
+    }
+    else if (quarter == 3)
+    {
+       System.Console.WriteLine("x < 0, y < 0"); 
+    }
+    else if (quarter == 4)
+    {
+        System.Console.WriteLine("x > 0, y < 0");
+    }
+    else
+    {
+        System.Console.WriteLine("Нет такой четверти");
+    }
+}
+
+int quarter = InputNum("Введите номер четверти: ");
+QuarterAvailable(quarter);
+
---
+++ Task_example/Program.cs
@@ -0,0 +1,12 @@
+﻿// max и min значение int
+// вывести int max + 1
+
+int min = int.MinValue;
+int max = int.MaxValue;
+
+System.Console.WriteLine(min);
+System.Console.WriteLine(max);
+
+int maxPlusOne = max + 1;
+
+System.Console.WriteLine(maxPlusOne);
