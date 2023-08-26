27c28e8dc1f0155c0223073633c647e979be9f94
Olga <workpost.mkh@yandex.ru>
2023-08-27 01:51:54 +0300

    Task 3.1

---
+++ Task3.1/Program.cs
@@ -0,0 +1,23 @@
+﻿// // написать программу которая принимает на вход координаты двух точек и находит расстояние между ними 
+
+int[] InputCoordinate()
+{
+    int[] coor = new int[2];
+    System.Console.Write("Введите координату X: ");
+    coor[0] = int.Parse(Console.ReadLine()!);
+    System.Console.Write("Введите координату Y: ");
+    coor[1] = int.Parse(Console.ReadLine()!);
+    return coor;
+
+}
+double Distanse(int [] coordA, int [] coordB)
+{
+    double dist = Math.Sqrt(Math.Pow(coordA [0] - coordB [0], 2)+ Math.Pow(coordA [1] - coordB [1], 2));
+    return dist;
+}
+
+int [] coordA = InputCoordinate();
+int [] coordB = InputCoordinate();
+
+double resultate = Distanse(coordA, coordB);
+System.Console.WriteLine(resultate);
