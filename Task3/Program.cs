5a23cd4aa0002d3a4956defa5a72725c3b89ef4f
Olga <workpost.mkh@yandex.ru>
2023-08-27 01:29:47 +0300

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// написать программу которая принимает на вход координаты двух точек и находит расстояние между ними в пространстве
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+double PointDistance(int AX, int AY, int BX, int BY)
+{
+    double dist = 0;
+    dist = Math.Sqrt(Math.Pow(AX - BX, 2) + Math.Pow(AY - BY, 2));
+    return dist;
+}
+
+int AX = InputNum("Введите значение оси координат x для точки a: ");
+int AY = InputNum("Введите значение оси координат y для точки A: ");
+int BX = InputNum("Введите значение оси координат x для точки B: ");
+int BY = InputNum("Введите значение оси координат y для точки B: ");
+System.Console.WriteLine(PointDistance(AX, AY, BX, BY));
